a0b5f183ad07586bf47d0524e994da90124d4635
Luu Thy Thy <124854511+ThyLuu@users.noreply.github.com>
2025-10-18 23:11:08 +0700

    Create ValidPalindrome_Easy.ts

---
+++ ValidPalindrome_Easy.ts
@@ -0,0 +1,17 @@
+function isPalindrome(s: string): boolean {
+  if(s === '') return true
+
+  s = s.toLowerCase().replace(/[^a-z0-9]/g, '')
+
+  let i = 0
+  let j = s.length - 1
+
+  while (i < j) {
+    if(s[i] !== s[j]) return false
+
+    i++
+    j--
+  }
+
+  return true
+}
